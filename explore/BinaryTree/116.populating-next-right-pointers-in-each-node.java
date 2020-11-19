bdf630b6b462ed743d6ae1f1ac12ed3b3d6b0dcc
junjie.fu_U+ <junjie.fu@united-imaging.com>
2020-11-19 15:27:59 +0800

    层级遍历next指针1

---
+++ explore/BinaryTree/116.populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode id=116 lang=java
+ *
+ * [116] Populating Next Right Pointers in Each Node
+ *
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+ *
+ * algorithms
+ * Medium (46.84%)
+ * Likes:    2715
+ * Dislikes: 151
+ * Total Accepted:    426.2K
+ * Total Submissions: 894.7K
+ * Testcase Example:  '[1,2,3,4,5,6,7]'
+ *
+ * You are given a perfect binary tree where all leaves are on the same level,
+ * and every parent has two children. The binary tree has the following
+ * definition:
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * You may only use constant extra space.
+ * Recursive approach is fine, you may assume implicit stack space does not
+ * count as extra space for this problem.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,2,3,4,5,6,7]
+ * Output: [1,#,2,3,#,4,5,6,7,#]
+ * Explanation: Given the above perfect binary tree (Figure A), your function
+ * should populate each next pointer to point to its next right node, just like
+ * in Figure B. The serialized output is in level order as connected by the
+ * next pointers, with '#' signifying the end of each level.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the given tree is less than 4096.
+ * -1000 <= node.val <= 1000
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+
+    Node ROOT = new Node();
+    public Node connect(Node root) {
+        connectByLevel(root, 1);
+        return root;
+    }
+
+    // recursive by level
+    public void connectByLevel(Node node,int level){
+        if (node == null || node.left == null) {
+            return;
+        }
+        ROOT = node;
+        for (int i = 0; i < (int)Math.pow(2, level-1); i++) {
+            node.left.next = node.right;
+            if (node.next != null) {
+                node.right.next=node.next.left;
+                node = node.next;
+            }
+        }
+       connectByLevel(ROOT.left, level+1);
+    }
+}
+// @lc code=end
+
